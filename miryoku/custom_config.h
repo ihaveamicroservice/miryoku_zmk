fdbcc3378997b45f44bdc4a23be97970ef8f1abf
Duss Kevin <kevin.duss1@sanitas.com>
2026-03-21 18:58:59 +0100

    add chocofi build config

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,20 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+// Override the VI-style Mouse layer
+// - Rearrange the clipboard keys
+// - Replace LEFT with 10h, DOWN with 10j, UP with 10k, RIGHT with 10l
+// The macros are defined in miryoku_custom_macros.dtsi
+#define MIRYOKU_LAYER_MOUSE \
+U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_CPY,             U_UND,             U_CUT,             U_RDO,             U_PST,             \
+&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &u_scroll_left_10, &u_scroll_down_10, &u_scroll_up_10,   &u_scroll_right_10,U_NU,              \
+U_NA,              &kp RALT,          &u_to_U_SYM,       &u_to_U_MOUSE,     U_NA,              U_WH_L,            U_WH_D,            U_WH_U,            U_WH_R,            U_NU,              \
+U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              U_BTN2,            U_BTN1,            U_BTN3,            U_NP,              U_NP
+
+// Override the VI-style Nav layer
+// - Rearrange the clipboard keys
+#define MIRYOKU_LAYER_NAV \
+U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_CPY,             U_UND,             U_CUT,             U_RDO,             U_PST,             \
+&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &kp LEFT,          &kp DOWN,          &kp UP,            &kp RIGHT,         &u_caps_word,      \
+U_NA,              &kp RALT,          &u_to_U_NUM,       &u_to_U_NAV,       U_NA,              &kp HOME,          &kp PG_DN,         &kp PG_UP,         &kp END,           &kp INS,           \
+U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp RET,           &kp BSPC,          &kp DEL,           U_NP,              U_NP
